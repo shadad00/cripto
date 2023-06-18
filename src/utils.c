d053b64c7397beb2074b4ed9028b3d541881cf75
shadad00 <shadad@itba.edu.ar>
2023-06-18 18:49:18 -0300

    fix: fprintf

--- src/utils.c
+++ src/utils.c
@@ -9,7 +9,7 @@
 
 struct params * validateParams(int argc, char * argv[]){
     if(argc != EXPECTED_PARAMS){
-        fprintf("The provided parameters are incorrect. \n");
+        printf("The provided parameters are incorrect. \n");
         exit(EXIT_FAILURE);
     }
     struct params * params = malloc(sizeof (struct params));
@@ -18,7 +18,7 @@ struct params * validateParams(int argc, char * argv[]){
     strcpy(params->file, argv[2]);
     params->k = atoi(argv[3]);
     if ( params->k < 3 || params->k > 8){
-        fprintf("The value given to k is not correct. \n");
+        printf("The value given to k is not correct. \n");
         exit(EXIT_FAILURE);
     }
     params->directory = malloc(strlen(argv[4]));
